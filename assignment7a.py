380cce42b1e8d4b992d2f189ead43ed5f2db9ea9
arpitasahool <37792496+arpitasahool@users.noreply.github.com>
2018-08-23 22:59:49 +0530

    Update assignment7a.py

--- assignment7a.py
+++ assignment7a.py
@@ -1,5 +1,5 @@
 #-Assignment-Dictionary
-#-Answer-1 values finding using loops
+#-ques1:- Create a user defined dictionary and get keys corresponding to the value using for loop. 
 a=eval(input('enter dictionary'))
 b=(input('enter the value'))
 for k,v in a.items():
@@ -8,7 +8,8 @@ for k,v in a.items():
 print(k)
 
 
-#-Answer-2 User defined nested dictionary
+#-ques2:- Create a dictionary and store student names and create nested dictionary to store the subject wise marks of every student.Print the marks of a given student from that dictionary for every subject. 
+#Hint: Use nested dictionary to store subjects marks.
 a={'arpita':{'maths':100,'physics':90,'chemistry':80},'asmita':{'maths':80,'physics':90,'chemistry':100},'bandita':{'maths':70,'physics':80,'chemistry':90}}
 b=input('enter name of student')
 for k,v in a.items():
